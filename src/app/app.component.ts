243f3d1535a4a047a7be33bea9f966916a3014f5
BOOPATHY-GIT <boopathy.21@gmail.com>
2020-07-03 12:07:52 +0200

    adding ngx-matomo instead of script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatomoInjector, MatomoTracker } from 'ngx-matomo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-starter';
   version = 'Angular version 9.1.9';
+
+  constructor(
+    private matomoInjector: MatomoInjector,
+    private matomoTracker: MatomoTracker
+  ) {
+    this.matomoInjector.init('https://boopathy-git.matomo.cloud/', 1);
+  }
+  ngOnInit() {
+    this.matomoTracker.setUserId('UserId');
+    this.matomoTracker.setDocumentTitle('ngx-Matomo Test');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatomoModule } from 'ngx-matomo';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/general/home/home.component';
@@ -13,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatomoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
